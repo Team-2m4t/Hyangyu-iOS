c31fb4c5c82521856849e66a138115614bd476d9
bsorinnn <bxxsxrxx@gmail.com>
2022-01-07 00:46:04 +0900

    [chore] 프로젝트 기초 세팅(#1)
      - 기초 프로젝트 생성
      - 폴더링
      - Cocoapods 설치
    
    Close: #1

---
+++ Hyangyu/Hyangyu/Sources/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hyangyu
+//
+//  Created by bsorinnn on 2022/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
