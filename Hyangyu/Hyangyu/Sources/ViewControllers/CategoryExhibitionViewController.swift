793f1541357893ddbc2de65888797ca4996561b3
abi-hong <68041042+abi-hong@users.noreply.github.com>
2022-01-13 10:19:18 +0900

    [feat] 카테고리 레이아웃 구현

---
+++ Hyangyu/Hyangyu/Sources/ViewControllers/CategoryExhibitionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryExhibitionViewController.swift
+//  Hyangyu
+//
+//  Created by 홍희수 on 2022/01/10.
+//
+
+import UIKit
+
+class CategoryExhibitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
