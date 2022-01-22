ea73e0f54a8e9faa0551c1d4e43dc3894a910467
bsorinnn <bxxsxrxx@gmail.com>
2022-01-23 03:51:49 +0900

    [feat] Constatns 추가 (#4)
    - 이미지, 폰트 Constants 추가

---
+++ Hyangyu/Hyangyu/Resources/Constants/Image.swift
@@ -0,0 +1,29 @@
+//
+//  Image.swift
+//  Hyangyu
+//
+//  Created by 배소린 on 2022/01/23.
+//
+
+import UIKit
+
+public enum Image {
+    
+    // icons for tabbar
+    public static let icHomeCustom = UIImage(named: "home")!
+    public static let icHomeSelected = UIImage(named: "home-fill")!
+    public static let icSearchCustom = UIImage(named: "search")!
+    public static let icSearchSelected = UIImage(named: "search-fill")!
+    public static let icCategoryCustom = UIImage(named: "category")!
+    public static let icCategorySelected = UIImage(named: "category-fill")!
+    public static let icMyPageCustom = UIImage(named: "myPage")!
+    public static let icMyPageSelected = UIImage(named: "myPage-fill")!
+    
+    
+    // icons
+    public static let icSetting = UIImage(named: "settingIcon")!
+    public static let backButton = UIImage(named: "backBtn")!
+    
+    
+    public static let userDefaultImage = UIImage(named: "userDefaultImage")!
+}
