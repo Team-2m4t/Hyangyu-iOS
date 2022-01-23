6e16fceaafb66b91322ff2ce645efb81b5d5d893
bsorinnn <bxxsxrxx@gmail.com>
2022-01-24 00:46:04 +0900

    [add] Info.plist 수정, UIView Extensions 설정 (#4)
    - Info.plist 사진 접근 권한 설정
    - UIExtensions 추가
    
    Close: #4

--- Hyangyu/Hyangyu/Sources/Extensions/UIView+Extensions.swift
+++ Hyangyu/Hyangyu/Sources/Extensions/UIView+Extensions.swift
@@ -8,6 +8,17 @@
 import Foundation
 import UIKit
 extension UIView {
+    
+    func makeRounded(radius: CGFloat) {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = radius
+    }
+    
+    func makeRoundedWithBorder(radius: CGFloat, color: CGColor, borderWith: CGFloat = 1) {
+        makeRounded(radius: radius)
+        self.layer.borderWidth = borderWith
+        self.layer.borderColor = color
+    }
 
     
     func dropShadowWithMaskLayer(rounded: CGFloat) {
