f13ee5bcd483458382d03b6c3420af3e7324793c
KilTae <loverpleasestayo0@gmail.com>
2022-01-24 19:13:34 +0900

    [feat] 리뷰 구현

---
+++ Hyangyu/Hyangyu/Sources/APIModels/HomeHashtagDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeHashtagDataModel.swift
+//  Hyangyu
+//
+//  Created by 길태연 on 2022/01/18.
+//
+
+import Foundation
+
+struct HomeHashtagDataModel {
+    
+    var coverName:String
+    
+}
