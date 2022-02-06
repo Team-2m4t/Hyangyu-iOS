91bfe1fb0d550f108c4292acdcfb90f6f93f4a72
bsorinnn <bxxsxrxx@gmail.com>
2022-02-06 17:48:05 +0900

    [Feat] 스토리보드 시작 변경 (#9)
    - 스토리보드 파일로 들어오지 않고 코드로 들어오도록 변경

--- Hyangyu/Hyangyu/Sources/AppDelegate.swift
+++ Hyangyu/Hyangyu/Sources/AppDelegate.swift
@@ -6,10 +6,22 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    // MARK: - Core Data
+    lazy var persistentContainer: NSPersistentContainer = {
+        // name: Core Data 만든 파일명 지정
+        let container = NSPersistentContainer(name: "RecentSearchTerm")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                fatalError("Unresolved error, \((error as NSError).userInfo)")
+            }
+        })
+        return container
+    }()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
--- Hyangyu/Hyangyu/Sources/ViewControllers/ViewController.swift
+++ Hyangyu/Hyangyu/Sources/ViewControllers/ViewController.swift
@@ -8,13 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let button = UIButton().then {
+        $0.setTitle("시작 버튼", for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        $0.setTitleColor(UIColor.white, for: .normal)
+        $0.backgroundColor = .primary
+        $0.layer.cornerRadius = 20
+        $0.clipsToBounds = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(button)
+        configureUI()
+        button.addTarget(self, action: #selector(pushToTabbar), for: .touchUpInside)
+    }
+    
+    private func configureUI() {
+        
+        button.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
     }
 
-    @IBAction func pushToTabbar(_ sender: Any) {
+    @objc func pushToTabbar(_ sender: UIButton) {
         let tabBarStoryboard = UIStoryboard(name: "Tabbar", bundle: nil)
                 guard let tabBarController = tabBarStoryboard.instantiateViewController(withIdentifier: "TabBarViewController") as? TabBarViewController else {
                     return
