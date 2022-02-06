3f2ef6e5d87f6b146a661d3ed88f744593d4dee3
KilTae <73571829+KilTae@users.noreply.github.com>
2022-02-06 21:53:31 +0900

    [chore] 주석삭제

---
+++ Hyangyu/Hyangyu/Sources/HashtagDetailDataModel.swift
@@ -0,0 +1,16 @@
+//
+//  HashtagDetailDataModel.swift
+//  Hyangyu
+//
+//  Created by 길태연 on 2022/01/19.
+//
+
+import Foundation
+struct HashtagDetailDataModel {
+    
+    var coverName:String
+    var title : String
+    var subtitle : String
+    var data : String
+    
+}
---
+++ Hyangyu/Hyangyu/Sources/ReviewDataModel.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct ReviewDataModel {
+    
+    var title: String
+    var review: String
+    var data: String
+    var scope: Double
+}
