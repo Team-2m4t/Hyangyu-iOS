b091662e9a72c9d0da4676a766d5498ba581ebf1
abi-hong <68041042+abi-hong@users.noreply.github.com>
2022-02-08 22:46:11 +0900

    [feat] 카테고리 탭 완성

--- Hyangyu/Hyangyu/Sources/ViewControllers/CategoryViewController.swift
+++ Hyangyu/Hyangyu/Sources/ViewControllers/CategoryViewController.swift
@@ -13,34 +13,34 @@ class CategoryViewController: UIViewController {
     var fair = "박람회"
     var festival = "페스티벌"
     
-//    // 화면 전환 스토리보드
-//    let detailCategoryStoryboard : UIStoryboard = UIStoryboard(name: "DetailCategoryTab", bundle: nil)
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//    
-//    // 화면 전환
-//    
-//    @IBAction func exhibitionButtonClicked(_ sender: Any) {
-//        guard let nextVC = detailCategoryStoryboard.instantiateViewController(identifier: "DetailCategoryViewController") as? DetailCategoryViewController else {return}
-//        
-//        nextVC.categoryTabTitle = exhibition // 카테고리 title 이름
-//        self.navigationController?.pushViewController(nextVC, animated: true)
-//    }
-//    
-//    @IBAction func fairButtonClicked(_ sender: Any) {
-//        guard let nextVC = detailCategoryStoryboard.instantiateViewController(identifier: "DetailCategoryViewController") as? DetailCategoryViewController else {return}
-//        
-//        nextVC.categoryTabTitle = fair // 카테고리 title 이름
-//        self.navigationController?.pushViewController(nextVC, animated: true)
-//    }
-//    
-//    @IBAction func festivalButtonClicked(_ sender: Any) {
-//        guard let nextVC = detailCategoryStoryboard.instantiateViewController(identifier: "DetailCategoryViewController") as? DetailCategoryViewController else {return}
-//        
-//        nextVC.categoryTabTitle = festival // 카테고리 title 이름
-//        self.navigationController?.pushViewController(nextVC, animated: true)
-//    }
+    // 화면 전환 스토리보드
+    let detailCategoryStoryboard : UIStoryboard = UIStoryboard(name: "CategoryDetailPage", bundle: nil)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // 화면 전환
+    
+    @IBAction func exhibitionButtonClicked(_ sender: Any) {
+        guard let nextVC = detailCategoryStoryboard.instantiateViewController(identifier: "HomeVC") as? HomeVC else {return}
+        
+        nextVC.categoryTabTitle = exhibition // 카테고리 title 이름
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @IBAction func fairButtonClicked(_ sender: Any) {
+        guard let nextVC = detailCategoryStoryboard.instantiateViewController(identifier: "HomeVC") as? HomeVC else {return}
+        
+        nextVC.categoryTabTitle = fair // 카테고리 title 이름
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @IBAction func festivalButtonClicked(_ sender: Any) {
+        guard let nextVC = detailCategoryStoryboard.instantiateViewController(identifier: "HomeVC") as? HomeVC else {return}
+        
+        nextVC.categoryTabTitle = festival // 카테고리 title 이름
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
 }
