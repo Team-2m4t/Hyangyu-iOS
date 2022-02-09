eb5820f6ef3a8b3c9459ea5e17b06bd1f8efa10e
KilTae <73571829+KilTae@users.noreply.github.com>
2022-02-09 12:12:39 +0900

    [fix] 이미지 url 연결

--- Hyangyu/Hyangyu/Sources/Cells/HomeImageCollectionViewCell.swift
+++ Hyangyu/Hyangyu/Sources/Cells/HomeImageCollectionViewCell.swift
@@ -10,11 +10,16 @@ import UIKit
 class HomeImageCollectionViewCell: UICollectionViewCell {
     static let identifier: String = "HomeImageCollectionViewCell"
     @IBOutlet weak var posterImageView: UIImageView!
+    
     func setData(imageName: String) {
-        if let image = UIImage(named: imageName) {
-            posterImageView.image = image
+        let url = URL(string: "https://image.tmdb.org/t/p/original\(imageName)")
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url!)
+            DispatchQueue.main.async {
+                self.posterImageView.image = UIImage(data: data!)
+            }
         }
-        
+                           
     }
     func setImage() {
         posterImageView.layer.cornerRadius = 70
