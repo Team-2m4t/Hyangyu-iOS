3434adc0dbf6ac1df2216a5e5f906ba200949788
bsorinnn <bxxsxrxx@gmail.com>
2022-02-09 18:34:22 +0900

    [Feat] 상세보기 VC 연결 (#28)
    - 상세보기 VC 연결

--- Hyangyu/Hyangyu/Sources/ViewControllers/DetailPageViewController.swift
+++ Hyangyu/Hyangyu/Sources/ViewControllers/DetailPageViewController.swift
@@ -7,15 +7,75 @@
 
 import UIKit
 
+
 class DetailPageViewController: UIViewController {
+    
+    // MARK: - @IBOutlets
+    
+    @IBOutlet weak var mainPosterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var startDateLabel: UILabel!
+    @IBOutlet weak var endDateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var holidayLabel: UILabel!
+    @IBOutlet weak var weekdayOpenLabel: UILabel!
+    @IBOutlet weak var weekdayCloseLabel: UILabel!
+    @IBOutlet weak var weekendOpenLabel: UILabel!
+    @IBOutlet weak var weekendCloseLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var siteLabel: UILabel!
+    
+    // MARK: - Properties
+    
+    private var event: [Event] = []
     
     let moreReviewStoryboard : UIStoryboard = UIStoryboard(name: "MoreReviewPage", bundle: nil)
     
     let writeReviewStoryboard : UIStoryboard = UIStoryboard(name: "WriteReviewPage", bundle: nil)
-
+    
+    // MARK: - Life Cycle
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initNavigationController()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI()
+    }
+    
+    private func initNavigationController() {
+        self.navigationController?.initWithBackButton()
+    }
+    
+    // MARK: - Functions
+    
+    func configure(with event: Event) {
+        print(event)
+        self.event.append(event)
+    }
+    
+    private func configureUI() {
+        if !mainPosterImageView.updateServerImage(event[0].photo1) {
+            mainPosterImageView?.image = UIImage(named: "exhibitionImg1")
+        }
+        titleLabel?.text = event[0].title
+        startDateLabel?.text = event[0].startDate
+        endDateLabel?.text = event[0].endDate
+        locationLabel?.text = event[0].location
+        holidayLabel?.text = event[0].holiday
+        weekdayOpenLabel?.text = event[0].weekdayOpen
+        weekdayCloseLabel?.text = event[0].weekdayClose
+        weekendOpenLabel?.text = event[0].weekendOpen
+        weekendCloseLabel?.text = event[0].weekendClose
+        priceLabel?.text = "\(event[0].price)"
+        siteLabel?.text = event[0].site
+        
+    }
+    //
+    
+    // MARK: - @IBAction
     
     @IBAction func writeReviewButtonClicked(_ sender: Any) {
         guard let writeReviewVC = writeReviewStoryboard.instantiateViewController(identifier: "WriteReviewViewController") as? WriteReviewViewController else {return}
