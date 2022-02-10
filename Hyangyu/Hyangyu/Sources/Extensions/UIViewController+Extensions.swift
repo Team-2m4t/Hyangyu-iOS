842c070cc16d7b189b0d0cab8714845111af4814
bsorinnn <bxxsxrxx@gmail.com>
2022-02-10 15:35:01 +0900

    [Add] 토스트메세지 보여주는 익스텐션 추가 (#42)
    - 토스트 메세지 보여주는 익스텐션 추가

--- Hyangyu/Hyangyu/Sources/Extensions/UIViewController+Extensions.swift
+++ Hyangyu/Hyangyu/Sources/Extensions/UIViewController+Extensions.swift
@@ -40,4 +40,23 @@ extension UIViewController {
         @objc func dismissKeyboard() {
             self.view.endEditing(true)
         }
+    
+    // 토스트 메세지
+    func showToast(message: String) {
+        // 토스트 위치
+        let toastLabel = UILabel(frame: CGRect(x: 30,
+                                               y: self.view.frame.size.height - 95,
+                                               width: self.view.frame.size.width - 60,
+                                               height: 40))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.text = message
+        toastLabel.textAlignment = .center
+        toastLabel.layer.cornerRadius = 12
+        toastLabel.clipsToBounds = true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 1.0, delay: 0.5,
+                       options: .curveEaseIn, animations: { toastLabel.alpha = 0.0 },
+                       completion: {_ in toastLabel.removeFromSuperview() })
+    }
 }
