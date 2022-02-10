1927ee7b233732197f4d8d9627e8c6cede674e80
bsorinnn <bxxsxrxx@gmail.com>
2022-02-10 18:16:26 +0900

    [Feat] 서치 검색바 & 서치 결과 서버 연결 (#47)
    - 서치 검색바 & 서치 결과 화면 서버 연결

--- Hyangyu/Hyangyu/Sources/ViewModels/SearchResultsTableViewCellViewModel.swift
+++ Hyangyu/Hyangyu/Sources/ViewModels/SearchResultsTableViewCellViewModel.swift
@@ -8,9 +8,12 @@
 import Foundation
 
 struct SearchResultsTableViewCellViewModel {
-    let title: String
-    let imageURL: String?
-    let startDate: String
-    let endDate: String
-    let location: String
+    let eventID: Int
+    let title, startDate, endDate, weekdayOpen: String
+    let weekdayClose, weekendOpen, weekendClose, location: String
+    let site, holiday, content: String
+    let photo1: String
+    let photo2, photo3: String?
+    let price: Int
+    let saved: Bool
 }
