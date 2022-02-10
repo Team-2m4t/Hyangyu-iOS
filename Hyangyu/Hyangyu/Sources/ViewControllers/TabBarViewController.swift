bd976cbc4786ab02828bcc9a7525ea358cbbba81
bsorinnn <bxxsxrxx@gmail.com>
2022-02-10 20:52:00 +0900

    [Fix] 탭바 오류 수정 (#2)

--- Hyangyu/Hyangyu/Sources/ViewControllers/TabBarViewController.swift
+++ Hyangyu/Hyangyu/Sources/ViewControllers/TabBarViewController.swift
@@ -14,7 +14,6 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabBarUI()
-        self.addCustomTabBarView()
     }
     
     override func viewDidLayoutSubviews() {
